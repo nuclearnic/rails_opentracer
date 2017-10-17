aba06ca1b5a00d81afbe077be4c1d071accea338
NuclearNic <nuclearnicdev@gmaill.com>
2017-10-17 14:44:13 +0200

    Instrument tracer to create Active Record spans automatically

--- lib/generators/templates/rails_opentracer_initializer.rb
+++ lib/generators/templates/rails_opentracer_initializer.rb
@@ -3,3 +3,5 @@ require 'Zipkin'
 require 'Rails'
 
 OpenTracing.global_tracer = Zipkin::Tracer.build(url: ENV['ZIPKIN_SERVICE_URL'], service_name: Rails.application.class.parent_name)
+
+ActiveRecord::RailsOpentracer.instrument(tracer: OpenTracing.global_tracer, active_span: -> { $active_span })
