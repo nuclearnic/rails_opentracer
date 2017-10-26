8c60ac058ebb41b769ca48323bf130ec1c2e81ba
NuclearNic <nuclearnicdev@gmaill.com>
2017-10-26 16:53:55 +0200

    Replace generators with Railtie

--- lib/generators/rails_opentracer/install_generator.rb
+++ lib/generators/rails_opentracer/install_generator.rb
@@ -6,10 +6,10 @@ module RailsOpentracer
       source_root File.expand_path("../../templates", __FILE__)
       desc "Creates rails_opentracer initializer for your application"
 
-      def copy_initializer
-        template "rails_opentracer_initializer.rb", "config/initializers/rails_opentracer.rb"
-        puts "Initializer rails_opentracer added successfully"
-      end
+      # def copy_initializer
+      #   template "rails_opentracer_initializer.rb", "config/initializers/rails_opentracer.rb"
+      #   puts "Initializer rails_opentracer added successfully"
+      # end
 
       # def copy_tracer_middleware
       #   template "tracer.rb", "app/middleware/tracer.rb"
--- lib/generators/templates/rails_opentracer_initializer.rb
+++ lib/generators/templates/rails_opentracer_initializer.rb
@@ -1,30 +1,30 @@
-require 'opentracing'
-require 'zipkin'
+# require 'opentracing'
+# require 'zipkin'
 
-class Zipkin::JsonClient
-  # temporary monkey patch for a HTTPS related issue,
-  # remove this once fix has been made to
-  def emit_batch(spans)
-    return if spans.empty?
+# class Zipkin::JsonClient
+#   # temporary monkey patch for a HTTPS related issue,
+#   # remove this once fix has been made to
+#   def emit_batch(spans)
+#     return if spans.empty?
 
-    http = Net::HTTP.new(@spans_uri.host, @spans_uri.port)
-    http.use_ssl = true if @spans_uri.scheme == 'https'
-    puts "@spans_uri: #{@spans_uri} request_uri #{@spans_uri.request_uri}"
-    request = Net::HTTP::Post.new(@spans_uri.request_uri, {
-      'Content-Type' => 'application/json'
-    })
-    request.body = JSON.dump(spans)
-    response = http.request(request)
+#     http = Net::HTTP.new(@spans_uri.host, @spans_uri.port)
+#     http.use_ssl = true if @spans_uri.scheme == 'https'
+#     puts "@spans_uri: #{@spans_uri} request_uri #{@spans_uri.request_uri}"
+#     request = Net::HTTP::Post.new(@spans_uri.request_uri, {
+#       'Content-Type' => 'application/json'
+#     })
+#     request.body = JSON.dump(spans)
+#     response = http.request(request)
 
-    if response.code != 202
-      STDERR.puts(response.body)
-    end
-  rescue => e
-    STDERR.puts("Error emitting spans batch: #{e.message}\n#{e.backtrace.join("\n")}")
-  end
-end
+#     if response.code != 202
+#       STDERR.puts(response.body)
+#     end
+#   rescue => e
+#     STDERR.puts("Error emitting spans batch: #{e.message}\n#{e.backtrace.join("\n")}")
+#   end
+# end
 
-if ENV.key?('ZIPKIN_SERVICE_URL')
-  OpenTracing.global_tracer = Zipkin::Tracer.build(url: ENV['ZIPKIN_SERVICE_URL'], service_name: Rails.application.class.parent_name)
-  ActiveRecord::RailsOpentracer.instrument(tracer: OpenTracing.global_tracer, active_span: -> { $active_span })
-end
+# if ENV.key?('ZIPKIN_SERVICE_URL')
+#   OpenTracing.global_tracer = Zipkin::Tracer.build(url: ENV['ZIPKIN_SERVICE_URL'], service_name: Rails.application.class.parent_name)
+#   ActiveRecord::RailsOpentracer.instrument(tracer: OpenTracing.global_tracer, active_span: -> { $active_span })
+# end
