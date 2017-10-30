41b788e2e1cbf839ab7fd0751bc4f2be5b959d49
Nicholas <NuclearNic@users.noreply.github.com>
2017-10-30 15:12:08 +0200

    Move initializers to middleware.rb (#3)

--- lib/rails_opentracer.rb
+++ lib/rails_opentracer.rb
@@ -1,9 +1,9 @@
 require 'rails_opentracer/active_record/rails_opentracer.rb'
 require 'rails_opentracer/span_helpers'
-require 'rails_opentracer/version'
 require 'rails_opentracer/middleware'
-require 'faraday'
+require 'rails_opentracer/version'
 require 'opentracing'
+require 'faraday'
 require 'zipkin'
 
 module RailsOpentracer
@@ -37,44 +37,8 @@ module RailsOpentracer
     yield if block_given?
     @span.finish
   end
-  # BELOW WE ARE ATTEMPTING TO NOT USE GENERATORS SO THIS
-  # SHOULD EVENTUALLY GO INTO A SEPERATE FILE IF IT WORKS
-  class Railtie < Rails::Railtie
-    initializer 'rails_opentracer.configure_rack_middleware' do
-      Rails.application.middleware.use RailsOpentracer::Middleware
-    end
-    initializer 'rails_opentracer.configure_initializer' do
-      # temporary monkey patch for a HTTPS related issue,
-      # remove this once fix has been made to
-      class ::Zipkin::JsonClient
-        def emit_batch(spans)
-          return if spans.empty?
-      
-          http = Net::HTTP.new(@spans_uri.host, @spans_uri.port)
-          http.use_ssl = true if @spans_uri.scheme == 'https'
-          # puts "@spans_uri: #{@spans_uri} request_uri #{@spans_uri.request_uri}"
-          request = Net::HTTP::Post.new(@spans_uri.request_uri, {
-            'Content-Type' => 'application/json'
-          })
-          request.body = JSON.dump(spans)
-          response = http.request(request)
-      
-          if response.code != 202
-            STDERR.puts(response.body)
-          end
-        rescue => e
-          STDERR.puts("Error emitting spans batch: #{e.message}\n#{e.backtrace.join("\n")}")
-        end
-      end
-      if ENV.key?('ZIPKIN_SERVICE_URL')
-        OpenTracing.global_tracer = Zipkin::Tracer.build(url: ENV['ZIPKIN_SERVICE_URL'], service_name: Rails.application.class.parent_name)
-        ActiveRecord::RailsOpentracer.instrument(tracer: OpenTracing.global_tracer, active_span: -> { $active_span })
-      end
-    end
-  end
 
   private
-
   def denilize(hash)
     hash.each { |k, _v| hash[k] = '' if hash[k].nil? }
   end
