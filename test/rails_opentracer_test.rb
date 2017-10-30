6622ff81d8c6c17dffb471a1452d0876ee32a690
NuclearNic <nuclearnicdev@gmaill.com>
2017-10-30 16:23:01 +0200

    Add rails dependency for tests

--- test/rails_opentracer_test.rb
+++ test/rails_opentracer_test.rb
@@ -1,11 +1,38 @@
 require "test_helper"
-
 class RailsOpentracerTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RailsOpentracer::VERSION
-  end
+  # def test_child_of_using_span
+  #   span = OpenTracing::Tracer.new.start_span('operation_name')
+  #   reference = OpenTracing::Reference.child_of(span)
+  #   assert_equal reference.type, OpenTracing::Reference::CHILD_OF
+  #   assert_equal reference.context, span.context
+  # end
 
-  def test_it_does_something_useful
-    assert false
-  end
+  # def test_child_of_using_span_context
+  #   context = OpenTracing::Tracer.new.start_span('operation_name').context
+  #   reference = OpenTracing::Reference.child_of(context)
+  #   assert_equal reference.type, OpenTracing::Reference::CHILD_OF
+  #   assert_equal reference.context, context
+  # end
+
+  # def test_follows_from_using_span
+  #   span = OpenTracing::Tracer.new.start_span('operation_name')
+  #   reference = OpenTracing::Reference.follows_from(span)
+  #   assert_equal reference.type, OpenTracing::Reference::FOLLOWS_FROM
+  #   assert_equal reference.context, span.context
+  # end
+
+  # def test_follows_from_using_span_context
+  #   context = OpenTracing::Tracer.new.start_span('operation_name').context
+  #   reference = OpenTracing::Reference.follows_from(context)
+  #   assert_equal reference.type, OpenTracing::Reference::FOLLOWS_FROM
+  #   assert_equal reference.context, context
+
+  # def test_that_it_has_a_version_number
+  #   refute_nil ::RailsOpentracer::VERSION
+  # end
+
+  # def test_it_does_something_useful
+  #   assert true
+  #   # assert false
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
+require "rails/all"
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "rails_opentracer"
-
 require "minitest/autorun"
